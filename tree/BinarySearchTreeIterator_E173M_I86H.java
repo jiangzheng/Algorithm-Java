287d1e8bb60dba1f315dbbe0d45613b59dcc186a
jiangzheng <zhengjiang7@gmail.com>
2016-08-30 17:11:56 -0400

    Update BinarySearchTreeIterator_E173M_I86H.java

--- tree/BinarySearchTreeIterator_E173M_I86H.java
+++ tree/BinarySearchTreeIterator_E173M_I86H.java
@@ -65,8 +65,8 @@ public class BinarySearchTreeIterator_E173M_I86H {
  * Implement an iterator over a binary search tree (BST). 
  * Your iterator will be initialized with the root node of a BST.
  * Calling next() will return the next smallest number in the BST.
- * Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, 
- *   where h is the height of the tree.
+ * Note: 
+ * - next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
  * Your BSTIterator will be called like this:
  * BSTIterator i = new BSTIterator(root);
  * while (i.hasNext())
