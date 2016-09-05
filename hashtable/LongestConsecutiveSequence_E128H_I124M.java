b598cdb83df70a01ef78bc770c979675daeaed1e
jiangzheng <zhengjiang7@gmail.com>
2016-09-04 23:09:12 -0400

    Update LongestConsecutiveSequence_E128H_I124M.java

--- hashtable/LongestConsecutiveSequence_E128H_I124M.java
+++ hashtable/LongestConsecutiveSequence_E128H_I124M.java
@@ -41,7 +41,7 @@ public class LongestConsecutiveSequence_E128H_I124M {
 		System.out.println(obj.longestConsecutive(new int[]{}));	// -1
 		
 		int[] a = {100, 4, 200, 1, 3, 2};
-		System.out.println(obj.longestConsecutive(a));
+		System.out.println(obj.longestConsecutive(a));	// 4
 	}
 }
 
