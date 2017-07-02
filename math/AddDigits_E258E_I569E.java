fccbc728ad3d4b89c05b58c5c5c56a77ee6841a8
jiangzheng <zhengjiang7@gmail.com>
2017-07-02 09:32:08 -0400

    Update and rename AddDigits_E258E.java to AddDigits_E258E_I569E.java

--- math/AddDigits_E258E_I569E.java
+++ math/AddDigits_E258E_I569E.java
@@ -1,6 +1,6 @@
 package math;
 
-public class AddDigits_E258E {
+public class AddDigits_E258E_I569E {
 	/**
 	 * Straightforward.
 	 * Time: O(?); Space: O(1)
@@ -31,7 +31,7 @@ public class AddDigits_E258E {
 	}
 	
 	public static void main(String[] args) {
-		AddDigits_E258E obj = new AddDigits_E258E();
+		AddDigits_E258E_I569E obj = new AddDigits_E258E_I569E();
 		System.out.println(obj.addDigits1(-1));	// 0
 		
 		System.out.println(obj.addDigits1(6));	// 6
@@ -43,6 +43,7 @@ public class AddDigits_E258E {
 
 /**
  * LeetCode #258
+ * LintCode #569
  * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
  * For example: Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
  * Follow up: Could you do it without any loop/recursion in O(1) runtime?
